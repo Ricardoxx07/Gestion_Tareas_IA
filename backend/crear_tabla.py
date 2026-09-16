d7dafff44478accdf9692d87e06b25f05d04f68d
Ricardo <ricardo.chimoy2001@gmail.com>
2026-09-16 10:51:49 -0500

    chore: excluir archivos internos del repositorio

--- backend/crear_tabla.py
+++
@@ -1,9 +0,0 @@
-from database.database import engine, Base
-
-#Es imprescindible importar el modelo aquí para que Base conozca su existencia.
-from models.tarea_db import TareaDB
-
-#Lee todos los modelos registrados en Base y genera en PostgreSQL la sentencia CREATE TABLE IF NOT EXISTS tareas (...).
-Base.metadata.create_all(bind=engine)
-
-print("Tabla creada correctamente")
